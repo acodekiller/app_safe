d2efd49a5de14c752ff695ea49553acf2d5cbfe4
LinJie <1351503414@qq.com>
2023-05-27 20:40:10 +0800

    XXE注入

--- src/main/java/com/lin/controller/TestController.java
+++ src/main/java/com/lin/controller/TestController.java
@@ -3,10 +3,15 @@ package com.lin.controller;
 import com.lin.pojo.User;
 import org.apache.commons.io.IOUtils;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.WebUtils;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
 /**
@@ -80,5 +85,48 @@ public class TestController {
 
     }
 
+    private final static String USERNAME = "admin";
+    private final static String PASSWORD = "root";
+
+    @PostMapping("/login")
+    public void login(HttpServletRequest request,HttpServletResponse response) throws Exception{
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+//        factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl",true);
+        DocumentBuilder builder;
+        String result = "";
+        try{
+            builder = factory.newDocumentBuilder();
+            Document document = builder.parse(request.getInputStream());
+            String username = getValueByTagName(document,"username");
+            String password = getValueByTagName(document,"password");
+            if(username.equals(USERNAME) && password.equals(PASSWORD)){
+                result = String.format("<result><code>%d</code><msg>%s</msg></result>",1,username);
+            }else{
+                result = String.format("<result><code>%d</code><msg>%s</msg></result>",0,username);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            result = String.format("<result><code>%d</code><msg>%s</msg></result>",3,e.getMessage());
+        }
+        response.setContentType("text/xml;charset=UTF-8");
+        try {
+            response.getWriter().append(result);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public static String getValueByTagName(Document doc, String tagName){
+        if(doc == null || tagName.equals(null)){
+            return "";
+        }
+        NodeList pl = doc.getElementsByTagName(tagName);
+        if(pl != null && pl.getLength() > 0){
+            return pl.item(0).getTextContent();
+        }
+        return "";
+    }
+
 }
-//输入参数：C:%26calc
+//命令注入输入参数：C:%26calc
